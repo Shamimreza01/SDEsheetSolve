b994caf63c7872ab64a1b7f11461cf39a94c9548
shamim reza <srshamimreza97903116@gmail.com>
2024-02-26 16:18:08 +0600

    modified Find the duplicate one problem solution , add floyed algorithm

--- Arrays/010_L287_Find_the_dublicate_number.cpp
+++ Arrays/010_L287_Find_the_dublicate_number.cpp
@@ -1,3 +1,5 @@
+// optimize but it use eXtra space. TC=> O(n) SC=>O(n)
+/*
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
@@ -11,5 +13,25 @@ public:
          }
       }
       return result;  
+    }
+};
+*/ 
+// but we also optimized it using cycle in linked list or floyed algorithm 
+// TC=> O(n)  SC=>O(1)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+      int slow=0,fast=0;
+      while(true){
+          slow=nums[slow];
+          fast=nums[nums[fast]];
+          if(slow==fast) break;
+      }
+      int slow2=0;
+      while(slow!=slow2){
+          slow=nums[slow];
+          slow2=nums[slow2];
+      }
+      return slow;
     }
 };
