9fd99c27c15df226ff7b44cc2941e68b88140056
shamim reza <srshamimreza97903116@gmail.com>
2024-03-09 21:33:45 +0600

    solve leetcode 160 intersection point of two linked list

---
+++ LinkedList/031_L160_Intersection_of_two_linked_list.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* temp1=headA,*temp2=headB;
+        int l1=0,l2=0;
+        while(temp1){
+            temp1=temp1->next;
+            l1++;
+        }
+        while(temp2){
+            temp2=temp2->next;
+            l2++;
+        }
+        int diff=(l1>=l2)?l1-l2:l2-l1;
+        temp1=headA,temp2=headB;
+        while(diff--){
+            if(l1>l2){
+                temp1=temp1->next;
+            }else if(l1<l2){
+                temp2=temp2->next;
+            }
+        }
+     while(temp1||temp2){
+         if(temp1==temp2) return temp1;
+         temp1=temp1->next;
+         temp2=temp2->next;
+     }
+    return NULL;  
+    }
+};
